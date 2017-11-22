9aa1a58dcf2f55352477d0a8a77a75646f3a69ea
Sergey Kravchenko <krawa@krawa.net>
2017-11-21 18:43:34 -0800

    Cleanup methods

--- apps/kube-web/imports/api/kube.js
+++ apps/kube-web/imports/api/kube.js
@@ -9,47 +9,9 @@ export const Kube = {};
 
 const ctl = new KubeController();
 
-//==============================================================================
-const k8s = require('k8s');
-//const deployment = require('../../../#shared/deployment.js');
-//const service = require('../#shared/service.js');
-
-const apiConfig = {
-  'endpoint': Meteor.settings.ENDPOINT,
-  'version': '/api/v1',
-  'strictSSL': false,
-  'auth': {
-    'username': Meteor.settings.USERNAME,
-    'password': Meteor.settings.PASSWORD,
-  }
-};
-
-const label       = 'web';
-const maxReplicas = 5;
-
-//==============================================================================
-const apiExtConfig = JSON.parse(JSON.stringify(apiConfig));
-apiExtConfig.version = '/apis/extensions/v1beta1';
-
-const apiExt = k8s.api(apiExtConfig);
-
 if (Meteor.isServer) {
   Meteor.methods({
-    'kube.index'() {
-      /*
-      const fut = new Future();
-
-      apiExt.get('namespaces/default/deployments')
-        .then(data => {
-          console.log(data);
-          fut['return'](data);
-        })
-        .catch(err => console.log(err));
-
-      return fut.wait();
-      */
-      return ctl.index();
-    },
+    'kube.index'() {return ctl.index()},
     'kube.create'() {ctl.create()},
     'kube.delete'() {ctl.delete()},
     'kube.scale'(replicas) {ctl.scale(replicas)}
